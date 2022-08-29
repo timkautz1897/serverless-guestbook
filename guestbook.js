51db35e5eef4f152aedb4a989947c212ec618f5e
timkautz1897 <51714876+timkautz1897@users.noreply.github.com>
2022-08-29 12:29:36 +0200

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://b4775cf4.eu-de.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
